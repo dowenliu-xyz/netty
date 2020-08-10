317e280db50b2ba6d737dcadb27ea56a066834bc
dowenliu-xyz <hawkdowen@hotmail.com>
2020-08-10 17:09:14 +0800

    Deal with style-check

--- example/src/main/java/io/netty/example/echo/EchoServer.java
+++ example/src/main/java/io/netty/example/echo/EchoServer.java
@@ -17,7 +17,11 @@ package io.netty.example.echo;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.PooledByteBufAllocator;
-import io.netty.channel.*;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioChannelOption;
